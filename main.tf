ae805ec1bc18737305f5d1f203da5aaf31b407e4
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-09 13:53:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,11 +40,11 @@ resource "aws_instance" "ourfirst" {
   key_name = "hello-key"
   user_data = <<-EOF
         sudo apt update
-        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-        apt-cache policy docker-ce 
-        sudo apt install docker-ce
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+        apt-cache policy docker-ce
+        sudo apt install docker-ce -y
         sudo systemctl status docker
         sudo chmod 777 /var/run/docker.sock
         docker -ps 
