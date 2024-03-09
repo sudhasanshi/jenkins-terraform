8b1dbb7f0a3c605c7c89d48c3a8e58d9a791744f
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-09 13:42:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
+#security group
 resource "aws_security_group" "webserver_access" {
         name = "webserver_access"
         description = "allow ssh and http"
@@ -38,7 +39,15 @@ resource "aws_instance" "ourfirst" {
   security_groups = ["${aws_security_group.webserver_access.name}"]
   key_name = "hello-key"
   user_data = <<-EOF
-        
+        sudo apt update
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+        apt-cache policy docker-ce 
+        sudo apt install docker-ce
+        sudo systemctl status docker
+        sudo chmod 777 /var/run/docker.sock
+        docker -ps 
   EOF
 
   tags = {
