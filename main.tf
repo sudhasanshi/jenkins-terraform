c1edaf8e246d838b904f727b880658cbabbd77a3
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-09 13:37:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,48 @@
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
 }
 
-resource "aws_instance" "name" {
-  ami = "ami-03bb6d83c60fc5f7c"
+resource "aws_security_group" "webserver_access" {
+        name = "webserver_access"
+        description = "allow ssh and http"
+        vpc_id      = "vpc-0c9f229e3b5927265"
+        ingress {
+                from_port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+                from_port = 22
+                to_port = 22
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-03bb6d83c60fc5f7c"
+  availability_zone = "ap-south-1a"
   instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.webserver_access.name}"]
+  key_name = "hello-key"
+  user_data = <<-EOF
+        
+  EOF
+
+  tags = {
+    Name  = "hello-docker"
+  }
+
 }
