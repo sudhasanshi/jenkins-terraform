2e26a7db384abf3b9838608c8f370d4baad2b5d4
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-09 15:31:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,18 +38,7 @@ resource "aws_instance" "ourfirst" {
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.webserver_access.name}"]
   key_name = "hello-key"
-  user_data = <<-EOF
-        #!/bin/bash
-        sudo apt update
-        sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-        apt-cache policy docker-ce
-        sudo apt install docker-ce -y
-        sudo systemctl status docker
-        sudo chmod 777 /var/run/docker.sock
-        docker -ps 
-  EOF
+  user_data = file("dockerfile.sh")
 
   tags = {
     Name  = "hello-docker"
